55fb3888a7e92ebd2b1621a83bf3c724e114a43c
Daniel <daniel.arnstrom@liu.se>
2022-04-16 15:05:35 +0200

    Add test for warm-starting and infeasible problem.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DAQP.jl
-Julia interface for the Quadratic Programming solver DAQP
+[![CI](https://github.com/darnstrom/DAQP.jl/workflows/CI/badge.svg)](https://github.com/darnstrom/DAQP.jl/actions)
+[![Code coverage](http://codecov.io/gh/darnstrom/DAQP.jl/graphs/badge.svg)](http://codecov.io/github/darnstrom/DAQP.jl)
+Julia interface for the Quadratic Programming solver DAQP.
--- test/runtests.jl
+++ test/runtests.jl
@@ -20,33 +20,19 @@ tol = 1e-4
 end
 
 @testset "Quadprog (JL)" begin
-  for nQP in 1:25
+  qpj = DAQP.QPj()
+  for nQP in 1:23
 	xref,H,f,A,bupper,blower,sense = generate_test_QP(20,100,0,16,1e2);
-	x,fval,exitflag,info = DAQP.daqp_jl(H,f,[A;-A],[bupper;-blower],[sense;sense],Int64[]);
+	x,lam,AS,J,iter= DAQP.daqp_jl(H,f,[A;-A],[bupper;-blower],[sense;sense],Int64[]);
 	@test norm(xref-x) < tol;
   end
-end
-
-@testset "Model interface" begin
-  # Setup model and solve problem
-  d = DAQP.Model()
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
-  DAQP.setup(d,H,f,A,bupper,blower,sense)
-  x,fval,exitflag,info = DAQP.solve(d)
-  @test norm(xref-x) < tol
-
-  # Test access settings
-  s = DAQP.settings(d)
-  @test s.primal_tol==1e-6
-  DAQP.settings(d,Dict(:primal_tol=>1e-5))
-  s = DAQP.settings(d)
-  @test s.primal_tol==1e-5
-  
-  # Update existing model with new problem
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
-  DAQP.update(d,H,f,A,bupper,blower,sense)
-  x,fval,exitflag,info = DAQP.solve(d)
-  @test norm(xref-x) < tol
+  # Test warm start
+  xref,H,f,A,bupper,blower,sense = generate_test_QP(20,100,0,16,1e2);
+  x,lam,AS,J,iter= DAQP.daqp_jl(H,f,[A;-A],[bupper;-blower],[sense;sense],Int64[1]);
+  @test norm(xref-x) < tol;
+  # Test infeasible problem
+  x,lam,AS,J,iter= DAQP.daqp_jl([1.0 0; 0 1],zeros(2),[1.0 0;-1 0],[1;-2],zeros(Cint,2),Int64[]);
+  @test isinf(J)
 end
 
 @testset "Model interface" begin
