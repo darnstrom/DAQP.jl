74f8cb0f7ae96a8ff454f4c243cf32721a53b907
Daniel Arnström <daniel.arnstrom@liu.se>
2022-07-28 12:54:40 +0200

    Update constants.

--- src/constants.jl
+++ src/constants.jl
@@ -3,11 +3,15 @@ const ACTIVE = 1
 const LOWER = 2 
 const IMMUTABLE= 4
 const SOFT= 8 
+const BINARY= 16 
 
 # Exit Flags
-const flag2status= Dict{Int,Symbol}(1 => :Optimal,
+const flag2status= Dict{Int,Symbol}(2 => :Soft_Optimal,
+                                    1 => :Optimal,
 								   -1 => :Primal_Infeasible,
 								   -2 => :Cycling,
 								   -3 => :Unbounded,
-								   -4 => :Iteration_Limit
+								   -4 => :Iteration_Limit,
+                                   -5 => :Nonconvex_Problem,
+                                   -6 => :Initial_Overdetermined
 								   )
