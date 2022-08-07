94abbb7901a54f8b98aec670df2ff4b072d8d72e
Daniel Arnström <daniel.arnstrom@liu.se>
2022-08-07 21:12:04 +0200

    Add MOI tests and move old tests into core_test.jl

---
+++ test/MOI_wrapper.jl
@@ -0,0 +1,79 @@
+module TestMOIDAQP
+
+import DAQP 
+using MathOptInterface
+using Test
+
+const MOI = MathOptInterface
+
+const OPTIMIZER = MOI.instantiate(
+    MOI.OptimizerWithAttributes(DAQP.Optimizer),
+)
+
+const BRIDGED = MOI.instantiate(
+    MOI.OptimizerWithAttributes(DAQP.Optimizer),
+    with_bridge_type = Float64,
+)
+
+# See the docstring of MOI.Test.Config for other arguments.
+const CONFIG = MOI.Test.Config(
+    # Modify tolerances as necessary.
+    atol = 1e-6,
+    rtol = 1e-6,
+    # Use MOI.LOCALLY_SOLVED for local solvers.
+    optimal_status = MOI.OPTIMAL,
+    # Pass attributes or MOI functions to `exclude` to skip tests that
+    # rely on this functionality.
+    exclude = Any[MOI.VariableName, MOI.delete],
+)
+
+"""
+    runtests()
+
+This function runs all functions in the this Module starting with `test_`.
+"""
+function runtests()
+    for name in names(@__MODULE__; all = true)
+        if startswith("$(name)", "test_")
+            @testset "$(name)" begin
+                getfield(@__MODULE__, name)()
+            end
+        end
+    end
+end
+
+"""
+    test_runtests()
+
+This function runs all the tests in MathOptInterface.Test.
+
+Pass arguments to `exclude` to skip tests for functionality that is not
+implemented or that your solver doesn't support.
+"""
+function test_runtests()
+    MOI.Test.runtests(
+        BRIDGED,
+        CONFIG,
+        exclude = [
+            "test_objective_qp_ObjectiveFunction_edge_cases", #requires support for singular H 
+        ],
+        exclude_tests_after = v"0.10.5",
+    )
+    return
+end
+
+"""
+    test_SolverName()
+
+You can also write new tests for solver-specific functionality. Write each new
+test as a function with a name beginning with `test_`.
+"""
+function test_SolverName()
+    @test MOI.get(DAQP.Optimizer(), MOI.SolverName()) == "DAQP"
+    return
+end
+
+end # module TestMOIDAQP 
+
+# This line at tne end of the file runs all the tests!
+TestMOIDAQP.runtests()
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,96 +1,10 @@
-using LinearAlgebra
-using Random
 using Test
-using DAQP
-include(joinpath(dirname(@__FILE__), "utils.jl"))
-
-nQPs = 100;
-n = 100; m = 500; ms = 50;
-nAct = 80
-kappa = 1e2
-tol = 1e-4
-		  	
-
-@testset "Quadprog (C )" begin
-  for nQP in 1:nQPs
-	xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa);
-	x,fval,exitflag,info = DAQP.quadprog(H,f,A,bupper,blower,sense);
-	@test norm(xref-x) < tol;
-  end
+@testset "Core" begin
+    include("core_tests.jl")
 end
 
-@testset "Quadprog (JL)" begin
-  qpj = DAQP.QPj()
-  for nQP in 1:22
-	xref,H,f,A,bupper,blower,sense = generate_test_QP(20,100,0,16,1e2);
-	x,lam,AS,J,iter= DAQP.daqp_jl(H,f,[A;-A],[bupper;-blower],[sense;sense],Int64[]);
-	@test norm(xref-x) < tol;
-  end
-  # Test warm start
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(20,100,0,16,1e2);
-  x,lam,AS,J,iter= DAQP.daqp_jl(H,f,[A;-A],[bupper;-blower],[sense;sense],Int64[1]);
-  @test norm(xref-x) < tol;
-  # Test infeasible problem
-  x,lam,AS,J,iter= DAQP.daqp_jl([1.0 0; 0 1],zeros(2),[1.0 0;-1 0],[1;-2],zeros(Cint,2),Int64[]);
-  @test isinf(J)
-  # Test unconstrained problem
-  x,lam,AS,J,iter= DAQP.daqp_jl((H=[1.0 0; 0 1], f=zeros(2),
-								 A=[1.0 0;0 1], b=[1;1],
-								 senses=zeros(Cint,2)),Int64[]);
-  @test isempty(AS)
+@testset "MOI" begin
+    include("MOI_wrapper.jl")
 end
 
-@testset "BnB" begin
-    nb = 10
-    ϵb= 1e-5
-    nMIQPs = 5
-    for nMIQP = 1:nMIQPs
-        M = randn(n,n)
-        H = M'*M
-        A = randn(m-ms,n);
-        bupper = 20*rand(m); blower = -20*rand(m) # ensure that origin is feasible
-        f = 100*randn(n); f[1:nb].=-abs.(f[1:nb]) # make it lucrative to avoid origin
-        # Make first nb variables binary
-        bupper[1:nb].=1
-        blower[1:nb].=0
-        sense = zeros(Cint,m)
-        sense[1:nb].=DAQP.BINARY
-        x,fval,exitflag,info = DAQP.quadprog(H,f,A,bupper,blower,sense);
-        @test exitflag == 1 # was able to solve problem
-        @test all((abs.(x[1:nb].-1.0).<ϵb) .| (abs.(x[1:nb]).<ϵb)) # is binary feasible
-    end
-end
 
-@testset "Model interface" begin
-  # Setup model and solve problem
-  d = DAQP.Model()
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
-  DAQP.setup(d,H,f,A,bupper,blower,sense)
-  x,fval,exitflag,info = DAQP.solve(d)
-  @test norm(xref-x) < tol
-  @test norm(H*x+[I(n)[1:ms,:];A]'*info.λ+f) < tol
-
-  # Test access settings
-  s = DAQP.settings(d)
-  @test s.primal_tol==1e-6
-  DAQP.settings(d,Dict(:primal_tol=>1e-5))
-  s = DAQP.settings(d)
-  @test s.primal_tol==1e-5
-  
-  # Update existing model with new problem
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
-  DAQP.update(d,H,f,A,bupper,blower,sense)
-  x,fval,exitflag,info = DAQP.solve(d)
-  @test norm(xref-x) < tol
-  DAQP.delete!(d); # Test manually running destructor
-end
-
-@testset "C LDP interface" begin
-  # Setup model and solve problem
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
-  p=DAQP.setup_c_workspace(n)
-  DAQP.init_c_workspace_ldp(p,A,bupper,blower,sense;max_radius=1.0) 
-  work = unsafe_load(Ptr{DAQP.Workspace}(p));
-  @test work.n == n
-  DAQP.free_c_workspace(p)
-end
