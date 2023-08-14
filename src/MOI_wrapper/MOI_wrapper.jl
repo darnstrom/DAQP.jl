e40bbab5d6a107d519881963f9866a7f9e2c9ed4
Daniel Arnström <daniel.arnstrom@liu.se>
2023-08-14 10:48:49 +0200

    Refactor set/get

--- src/MOI_wrapper/MOI_wrapper.jl
+++ src/MOI_wrapper/MOI_wrapper.jl
@@ -249,12 +249,23 @@ MOI.set(opt::Optimizer, param::MOI.RawOptimizerAttribute, value) =
 MOI.get(opt::Optimizer, ::MOI.ObjectiveBound) =
     (opt.has_results) ? MOI.get(opt, MOI.DualObjectiveValue()) : opt.settings.fval_bound
 
-MOI.supports(::Optimizer, ::MOI.VariablePrimalStart,::Type{MOI.VariableIndex},) = true
-MOI.set(optimizer::Optimizer,a::MOI.VariablePrimalStart, vi::MOI.VariableIndex, value) = value
-function MOI.get(opt::Optimizer, a::MOI.VariablePrimalStart, vi::MOI.VariableIndex)
-    MOI.check_result_index_bounds(opt, a)
-    return opt.info.x[vi.value]
+function MOI.set(
+    model::Optimizer,
+    ::MOI.VariablePrimalStart,
+    ::MOI.VariableIndex,
+    ::Union{Nothing,Float64},
+)
+    return
 end
+
+function MOI.get(
+    model::Optimizer,
+    ::MOI.VariablePrimalStart,
+    vi::MOI.VariableIndex,
+)
+    return model.info.x[vi.value]
+end
+MOI.supports(::Optimizer, ::MOI.VariablePrimalStart,::Type{MOI.VariableIndex}) = true
 
 # not currently supported
 MOI.supports(::Optimizer, ::MOI.NumberOfThreads) = false
